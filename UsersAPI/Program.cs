ad6879eea0046d820f68db28b673030ab5d95b79
Christian Arellano <andres.9394@hotmail.com>
2023-04-16 04:28:26 -0600

    Actualización para middleware

--- UsersAPI/Program.cs
+++ UsersAPI/Program.cs
@@ -1,54 +1,17 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
 using Microsoft.EntityFrameworkCore;
 using UsersAPI;
 using UsersAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<UserDb>(opt => opt.UseInMemoryDatabase("UserList"));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization();
+
+//AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
 var app = builder.Build();
 
-app.MapGet("/users", async (UserDb db) => await db.Users.ToListAsync());
-
-app.MapGet("/users/active", async (UserDb db) => await db.Users.Where(t => t.IsActive).ToListAsync());
-
-app.MapGet("/users/{id}", async (int id, UserDb db) =>
-    await db.Users.FindAsync(id)
-        is User user
-            ? Results.Ok(user)
-            : Results.NotFound());
-
-app.MapPost("/users", async (User user, UserDb db) =>
-{
-    db.Users.Add(user);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/users/{user.Id}", user);
-});
-
-app.MapPut("/users/{id}", async (int id, User inputUser, UserDb db) =>
-{
-    var user = await db.Users.FindAsync(id);
-
-    if (user is null) return Results.NotFound();
-
-    user.Name = inputUser.Name;
-    user.IsActive = inputUser.IsActive;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/users/{id}", async (int id, UserDb db) =>
-{
-    if (await db.Users.FindAsync(id) is User user)
-    {
-        db.Users.Remove(user);
-        await db.SaveChangesAsync();
-        return Results.Ok(user);
-    }
-
-    return Results.NotFound();
-});
+app.MapGet("/", () => "Hello World!").RequireAuthorization();
 
 app.Run();
